3fac4dd5a756caa7615889d8b439fb50c41c2dab
0b5vr <0b5vr@0b5vr.com>
2026-07-08 18:58:58 +0900

    feat: update three revisions range
    not checked it work on all versions yet

--- src/threeRevisions.ts
+++ src/threeRevisions.ts
@@ -1,4 +1,5 @@
 export const threeRevisions = {
+  'r169': '169',
   'r170': '170',
   'r171': '171',
   'r172': '172',
@@ -7,8 +8,16 @@ export const threeRevisions = {
   'r175': '175',
   'r176': '176',
   'r177': '177',
+  'r178': '178',
+  'r179': '179',
+  'r180': '180',
+  'r181': '181',
+  'r182': '182',
+  'r183': '183',
+  'r184': '184',
+  'r185': '185',
 } as const;
 
 export type ThreeRevision = keyof typeof threeRevisions;
 
-export const DEFAULT_THREE_REVISION: ThreeRevision = 'r177';
+export const DEFAULT_THREE_REVISION: ThreeRevision = 'r185';
